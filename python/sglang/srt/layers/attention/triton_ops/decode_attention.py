1c799b5fa325604da089a8ae6e522cc789bab11c
Vinayak Gokhale <Vinayak.Gokhale@amd.com>
2024-12-28 19:03:35 +0000

    Change threshold to 576

--- python/sglang/srt/layers/attention/triton_ops/decode_attention.py
+++ python/sglang/srt/layers/attention/triton_ops/decode_attention.py
@@ -404,8 +404,9 @@ def _decode_grouped_att_m_fwd(
 ):
     Lk = k_buffer.shape[-1]
     Lv = v_buffer.shape[-1]
+    print(f"Lk = {Lk}"}
 
-    BLOCK = 16 if Lk > 256 else 32
+    BLOCK = 16 if Lk >= 576 else 32
 
     if Lk == 576:
         BLOCK_DMODEL = 512
