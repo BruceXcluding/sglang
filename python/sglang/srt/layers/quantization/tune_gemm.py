e93c9a95f444a54d4f229f5316fa970211d51385
yigex <yigex@amd.com>
2025-01-13 07:41:51 +0000

    Clang Format

--- python/sglang/srt/layers/quantization/tune_gemm.py
+++ python/sglang/srt/layers/quantization/tune_gemm.py
@@ -15,6 +15,7 @@ fp8_type_ = torch.float8_e4m3fnuz if is_hip_ else torch.float8_e4m3fn
 
 logger = logging.getLogger(__name__)
 
+
 @triton.jit
 def _per_token_group_quant_fp8(
     # Pointers to inputs and output
@@ -130,11 +131,13 @@ def get_cuda_autotune_config():
                     for num_warps in [4, 8]:
                         for group_size in [1, 4, 8, 16, 32]:
                             configs.append(
-                                triton.Config({
-                                    "BLOCK_SIZE_M": block_m,
-                                    "BLOCK_SIZE_N": block_n,
-                                    "BLOCK_SIZE_K": block_k,
-                                    "GROUP_SIZE_M": group_size},
+                                triton.Config(
+                                    {
+                                        "BLOCK_SIZE_M": block_m,
+                                        "BLOCK_SIZE_N": block_n,
+                                        "BLOCK_SIZE_K": block_k,
+                                        "GROUP_SIZE_M": group_size,
+                                    },
                                     num_warps=num_warps,
                                     num_stages=num_stages,
                                     waves_per_eu=waves_per_eu_range,
@@ -142,12 +145,11 @@ def get_cuda_autotune_config():
                             )
     return configs
 
+
 @triton.autotune(
     configs=get_cuda_autotune_config(),
-    key=['M', 'N', 'K'],
+    key=["M", "N", "K"],
 )
-
-
 @triton.jit
 def _w8a8_block_fp8_matmul(
     # Pointers to inputs and output
@@ -233,7 +235,6 @@ def _w8a8_block_fp8_matmul(
     tl.store(c_ptrs, c, mask=c_mask)
 
 
-
 def w8a8_block_fp8_matmul(
     A: torch.Tensor,
     B: torch.Tensor,
@@ -274,7 +275,6 @@ def w8a8_block_fp8_matmul(
     C_shape = A.shape[:-1] + (N,)
     C = A.new_empty(C_shape, dtype=output_dtype)
 
-
     def grid(META):
         return (
             triton.cdiv(M, META["BLOCK_SIZE_M"]) * triton.cdiv(N, META["BLOCK_SIZE_N"]),
@@ -305,22 +305,49 @@ def w8a8_block_fp8_matmul(
     return C
 
 
-
 def main():
-        #cannot TP
-    total = [(1536,7168), (3072,1536), (576,7168), (7168, 256), (7168, 2048), (4608, 7168), (7168, 2304), (512, 7168), (4096, 512)]
+    # cannot TP
+    total = [
+        (1536, 7168),
+        (3072, 1536),
+        (576, 7168),
+        (7168, 256),
+        (7168, 2048),
+        (4608, 7168),
+        (7168, 2304),
+        (512, 7168),
+        (4096, 512),
+    ]
     x_vals = []
-    for i in [1, 2, 4, 8, 16, 24, 32, 48, 64, 96, 128, 256, 512, 1024, 1536, 2048, 3072, 4096]:
+    for i in [
+        1,
+        2,
+        4,
+        8,
+        16,
+        24,
+        32,
+        48,
+        64,
+        96,
+        128,
+        256,
+        512,
+        1024,
+        1536,
+        2048,
+        3072,
+        4096,
+    ]:
         for t in total:
             x_vals.append((i,) + t)
 
     print(x_vals)
     print(len(x_vals))
 
-
-# @triton.testing.perf_report(configs)
-# def benchmark(M, N, K, provider):
-    def run(M,N,K):
+    # @triton.testing.perf_report(configs)
+    # def benchmark(M, N, K, provider):
+    def run(M, N, K):
         print()
         print(f"M: {M}, N: {N}, K: {K}")
         factor_for_scale = 1e-2
@@ -330,23 +357,33 @@ def main():
         block_size = [128, 128]
         out_dtype = torch.float16
 
-        A_fp32 = (torch.rand(M, K, dtype=torch.float32, device='cuda') - 0.5) * 2 * fp8_max
+        A_fp32 = (
+            (torch.rand(M, K, dtype=torch.float32, device="cuda") - 0.5) * 2 * fp8_max
+        )
         A_fp8 = A_fp32.clamp(min=fp8_min, max=fp8_max).to(torch.float8_e4m3fnuz)
 
-        B_fp32 = (torch.rand(N, K, dtype=torch.float32, device='cuda') - 0.5) * 2 * fp8_max
+        B_fp32 = (
+            (torch.rand(N, K, dtype=torch.float32, device="cuda") - 0.5) * 2 * fp8_max
+        )
         B_fp8 = B_fp32.clamp(min=fp8_min, max=fp8_max).to(torch.float8_e4m3fnuz)
 
         block_n, block_k = block_size[0], block_size[1]
         n_tiles = (N + block_n - 1) // block_n
-        k_tiles = (K + block_k -1) // block_k
+        k_tiles = (K + block_k - 1) // block_k
 
-        As = torch.rand(M, k_tiles, dtype=torch.float32, device='cuda') * factor_for_scale
-        Bs = torch.rand(n_tiles, k_tiles, dtype=torch.float32, device='cuda') * factor_for_scale
+        As = (
+            torch.rand(M, k_tiles, dtype=torch.float32, device="cuda")
+            * factor_for_scale
+        )
+        Bs = (
+            torch.rand(n_tiles, k_tiles, dtype=torch.float32, device="cuda")
+            * factor_for_scale
+        )
         w8a8_block_fp8_matmul(A_fp8, B_fp8, As, Bs, block_size, out_dtype)
-       
 
     for shape in x_vals:
         run(*shape)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
