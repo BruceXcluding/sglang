5655276e7deaff0e7017c3abd95be1d8eca411ee
zjli2013 <leezhengjiang@gmail.com>
2025-01-14 18:05:34 +0800

    clean sample instance and add 4 new instances

---
+++ sgl-kernel/src/sgl-kernel-amd/csrc/ck/gemm_a8w8/impl/a8w8_subblockwise_128x16x32x128_16x16_1x1_16x16_1x1_16x16_8x16x1_8x16x1_1x16x1x8_4x4x1_intrawave_v1.cu
@@ -0,0 +1,19 @@
+#include "../gemm_a8w8_subblock_common.cuh"
+
+template <typename DEDataType, typename ABDataType>
+torch::Tensor
+a8w8_subblockwise_128x16x32x128_16x16_1x1_16x16_intrawave_v1_kernel( torch::Tensor& XQ,
+    torch::Tensor& WQ,
+    torch::Tensor& x_scale,
+    torch::Tensor& w_scale,
+    torch::Tensor& Y) 
+{
+  int M = size_to_dim_(XQ.dim() - 1, XQ.sizes());
+  int N = WQ.size(0);
+  int K = WQ.size(1);
+  bool k_pad = (K % 128 != 0);
+  bool m_pad = (M % 128 != 0);
+  bool n_pad = (N % 128 != 0);
+  using DeviceGemmInstance = DeviceGemmHelper<128, 16, 32, 128, 16, 16, 1, 1, 16, 16, S<8, 16, 1>, S<8, 16, 1>, S<1, 16, 1, 8>, S<4, 4, 1>,  ck::BlockGemmPipelineScheduler::Intrawave, ck::BlockGemmPipelineVersion::v1> ;
+  return gemm_a8w8_subblockwise_impl<DeviceGemmInstance>(XQ, WQ, x_scale, w_scale, Y);
+}
--- sgl-kernel/src/sgl-kernel-amd/csrc/ck/gemm_a8w8/impl/a8w8_subblockwise_64x16x16x128_16x16_1x1_16x16_1x1_16x16_8x8x1_8x8x1_1x16x1x1_4x4x1_intrawave_v2.cu
+++ sgl-kernel/src/sgl-kernel-amd/csrc/ck/gemm_a8w8/impl/a8w8_subblockwise_64x16x16x128_16x16_1x1_16x16_1x1_16x16_8x8x1_8x8x1_1x16x1x1_4x4x1_intrawave_v2.cu
@@ -2,20 +2,18 @@
 
 template <typename DEDataType, typename ABDataType>
 torch::Tensor
-sample_instance( torch::Tensor& XQ,
+a8w8_subblockwise_128x16x32x128_16x16_1x1_16x16_intrawave_v1_kernel( torch::Tensor& XQ,
     torch::Tensor& WQ,
     torch::Tensor& x_scale,
     torch::Tensor& w_scale,
     torch::Tensor& Y) 
 {
-    // Check if this input needs to be padded.
   int M = size_to_dim_(XQ.dim() - 1, XQ.sizes());
   int N = WQ.size(0);
   int K = WQ.size(1);
   bool k_pad = (K % 128 != 0);
   bool m_pad = (M % 128 != 0);
   bool n_pad = (N % 128 != 0);
-  // TODO: add template arguments from best config list
-  using DeviceGemmInstance = DeviceGemmHelper<..>
+  using DeviceGemmInstance = DeviceGemmHelper<64, 16, 16, 128, 16, 16, 1, 1, 16, 16, S<8, 8, 1>, S<8, 8, 1>, S<1, 16, 1, 4>, S<4, 4, 1>,  ck::BlockGemmPipelineScheduler::Intrawave, ck::BlockGemmPipelineVersion::v2> ;
   return gemm_a8w8_subblockwise_impl<DeviceGemmInstance>(XQ, WQ, x_scale, w_scale, Y);
 }
