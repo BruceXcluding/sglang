ffef5670bb4035d58a1895a6d31501a4044eb61b
Bruce Xue <yigex@xilinx.com>
2025-02-21 10:51:12 +0800

    add mla dp linear flag

--- python/sglang/srt/server_args.py
+++ python/sglang/srt/server_args.py
@@ -147,6 +147,7 @@ class ServerArgs:
     disable_overlap_schedule: bool = False
     enable_mixed_chunk: bool = False
     enable_dp_attention: bool = False
+    enable_dp_linear: bool = False
     enable_ep_moe: bool = False
     enable_torch_compile: bool = False
     torch_compile_max_bs: int = 32
