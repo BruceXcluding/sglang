ac878c81c0265d4ab29f786eb40b85aa3e65da3c
yigex <yigex@amd.com>
2025-04-10 02:58:27 +0000

    add extend asm mla

--- python/sglang/srt/models/deepseek_v2.py
+++ python/sglang/srt/models/deepseek_v2.py
@@ -683,12 +683,6 @@ class DeepseekV2AttentionMLA(nn.Module):
                 and not forward_batch.forward_mode.is_draft_extend()
                 and sum(forward_batch.extend_prefix_lens_cpu) == 0
             )
-        elif _is_hip and get_bool_env_var("CK_MOE"):
-            return (
-                forward_batch.forward_mode.is_extend()
-                and not forward_batch.forward_mode.is_target_verify()
-                and not forward_batch.forward_mode.is_draft_extend()
-            )
         else:
             # Triton: Use normal computation for prefill and use weight absorption for extend/decode
             return (
